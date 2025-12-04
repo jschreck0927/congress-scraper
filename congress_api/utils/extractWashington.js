a2d7da9dd08d1a0cb368ff74616e2bfae45c327e
jschreck0927 <jschreck0927@gmail.com>
2025-12-04 07:11:49 -0500

    Update extractWashington.js

--- congress_api/utils/extractWashington.js
+++ congress_api/utils/extractWashington.js
@@ -1,48 +1,21 @@
-// Washington state federal delegation (House + Senate)
-const WA_MEMBERS = [
-  // House (10 districts)
-  "DelBene",     // WA-01
-  "Larsen",      // WA-02
-  "Gluesenkamp", // WA-03 (Perez)
-  "Perez",
-  "Newhouse",    // WA-04
-  "Baumgartner", // WA-05
-  "Randall",     // WA-06
-  "Jayapal",     // WA-07
-  "Schrier",     // WA-08
-  "Smith",       // WA-09
-  "Strickland",  // WA-10
-
-  // Senate
-  "Murray",
-  "Cantwell"
-];
-
-// Normalize for easy matching
-function isWashingtonMember(name = "") {
-  return WA_MEMBERS.some(last => name.includes(last));
-}
+// extractWashington.js
+//
+// This function modifies the bill object in-place,
+// adding WA-specific sponsor/cosponsor fields.
 
 export function extractWashington(bill) {
-  // Defaults
-  bill.hasWaSponsor = false;
-  bill.waSponsor = null;
-  bill.waCosponsors = [];
-  bill.waCosponsorCount = 0;
+  const WA_STATE = "WA";
 
-  // Check primary sponsor
-  if (bill.sponsor && isWashingtonMember(bill.sponsor.lastName || bill.sponsor.fullName)) {
+  // WA sponsor
+  if (bill.sponsor?.state === WA_STATE) {
     bill.hasWaSponsor = true;
     bill.waSponsor = bill.sponsor;
+  } else {
+    bill.hasWaSponsor = false;
+    bill.waSponsor = null;
   }
 
-  // Check cosponsors
-  if (Array.isArray(bill.cosponsors)) {
-    bill.waCosponsors = bill.cosponsors.filter(c =>
-      isWashingtonMember(c.lastName || c.fullName)
-    );
-    bill.waCosponsorCount = bill.waCosponsors.length;
-  }
-
-  return bill;
+  // WA cosponsors
+  bill.waCosponsors = bill.cosponsors.filter(c => c.state === WA_STATE);
+  bill.waCosponsorCount = bill.waCosponsors.length;
 }
