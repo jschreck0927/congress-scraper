c00aa89c140391d75812332ffbeb1d46dc8e172e
jschreck0927 <jschreck0927@gmail.com>
2025-12-03 20:53:02 -0500

    Update extractWashington.js

--- congress_api/utils/extractWashington.js
+++ congress_api/utils/extractWashington.js
@@ -1,18 +1,48 @@
-function extractWashington(bill) {
-  if (bill.sponsor && bill.sponsor.state === "WA") {
+// Washington state federal delegation (House + Senate)
+const WA_MEMBERS = [
+  // House (10 districts)
+  "DelBene",     // WA-01
+  "Larsen",      // WA-02
+  "Gluesenkamp", // WA-03 (Perez)
+  "Perez",
+  "Newhouse",    // WA-04
+  "Baumgartner", // WA-05
+  "Randall",     // WA-06
+  "Jayapal",     // WA-07
+  "Schrier",     // WA-08
+  "Smith",       // WA-09
+  "Strickland",  // WA-10
+
+  // Senate
+  "Murray",
+  "Cantwell"
+];
+
+// Normalize for easy matching
+function isWashingtonMember(name = "") {
+  return WA_MEMBERS.some(last => name.includes(last));
+}
+
+export function extractWashington(bill) {
+  // Defaults
+  bill.hasWaSponsor = false;
+  bill.waSponsor = null;
+  bill.waCosponsors = [];
+  bill.waCosponsorCount = 0;
+
+  // Check primary sponsor
+  if (bill.sponsor && isWashingtonMember(bill.sponsor.lastName || bill.sponsor.fullName)) {
     bill.hasWaSponsor = true;
     bill.waSponsor = bill.sponsor;
-  } else {
-    bill.hasWaSponsor = false;
-    bill.waSponsor = null;
   }
 
-  const waCos = (bill.cosponsors || []).filter(c => c.state === "WA");
-
-  bill.waCosponsors = waCos;
-  bill.waCosponsorCount = waCos.length;
+  // Check cosponsors
+  if (Array.isArray(bill.cosponsors)) {
+    bill.waCosponsors = bill.cosponsors.filter(c =>
+      isWashingtonMember(c.lastName || c.fullName)
+    );
+    bill.waCosponsorCount = bill.waCosponsors.length;
+  }
 
   return bill;
 }
-
-export { extractWashington };
