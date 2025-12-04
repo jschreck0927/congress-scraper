928c50e959cb27818333ed6581993426fe224297
jschreck0927 <jschreck0927@gmail.com>
2025-12-03 20:19:34 -0500

    Update extractWashington.js
    Add WA-only filter function

--- congress_api/utils/extractWashington.js
+++ congress_api/utils/extractWashington.js
@@ -1,1 +1,19 @@
+function extractWashington(bill) {
+  if (bill.sponsor && bill.sponsor.state === "WA") {
+    bill.hasWaSponsor = true;
+    bill.waSponsor = bill.sponsor;
+  } else {
+    bill.hasWaSponsor = false;
+    bill.waSponsor = null;
+  }
+
+  const waCos = (bill.cosponsors || []).filter(c => c.state === "WA");
+
+  bill.waCosponsors = waCos;
+  bill.waCosponsorCount = waCos.length;
+
+  return bill;
+}
+
+module.exports = { extractWashington };
 
